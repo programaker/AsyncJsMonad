a811af7e8be6a9246054dba6d5cf29dcc6c8b93c
programaker <programaker@gmail.com>
2016-06-29 23:48:00 -0300

    Async monadic module done - I guess...

--- async.js
+++ async.js
@@ -2,57 +2,75 @@
     'use strict';
 
     window.Async = {
-        create: create,
+        start: start,
         map: map,
         flatMap: flatMap
     };
 
 
-    /** create :: c -> Async a */
-    function create(config) {
-        //v1 return new Async(config, identity);
-        //v2 return new Async(config, null);
-        
-    }
-
-    /** map :: Async a -> (a -> b) -> Async b */
-    function map(asyncA, fn) {
-        //v1 new Async(asyncA.config, function(z){ return fn(asyncA.valueFn(z)) });
-        //v2 new Async(asyncA.config, fn(asyncA.value));
-        new Async(function(z){ return fn(asyncA.valueFn(z)) });
-    }
-
-    /** flatMap :: Async a -> (a -> Async b) -> Async b */
-    function flatMap(asyncA, fn) {
-        //v2 return fn(asyncA.value);
-        new Async(function(z) {
-            fn(asyncA.valueFn(z)).valueFn(?)
-        });
-    }
-
-    function run(config, fn) {
-        var async = new Async(fn);
+    /** start :: () -> Async () */
+    function start(config) {
+        var async = new Async();
 
         $.ajax(config).done(success).fail(error);
         function success(resp) {
-            async.value = async.valueFn(resp);
+            complete(async, resp);
         }
         function error(jqXHR, textStatus, errorThrown) {
             //???
         }
 
-        return async;
+        return async;        
+    }
+
+    /** map :: Async a -> (a -> b) -> Async b */
+    function map(asyncA, fn) {
+        var asyncB = new Async();
+        ready(asyncA, function(a){ complete(asyncB, fn(a)) });
+        return asyncB;
+    }
+
+    /** flatMap :: Async a -> (a -> Async b) -> Async b */
+    function flatMap(asyncA, fn) {
+        return flatten(map(asyncA, fn));
     }
 
 
-    /*v2
-    function Async(_config, _valueFn) {
-        this.config = _config;
-        this.valueFn = _valueFn;
-    }*/
+    function complete(async, value) {
+        async.value = value;
+        async.completed = true;
+        async.action(value);
+    }
+
+    function ready(async, fn) {
+        if (async.completed) {
+            fn(async.value);
+        } else {
+            async.action = fn;
+        }
+    }
+
+    /** flatten :: Async Async a -> Async a */
+    function flatten(nestedAsync) {
+        var flatAsync = new Async();
+
+        ready(nestedAsync, function(internalAsync) {
+            ready(internalAsync, function(internalAsyncValue) {
+                complete(flatAsync, internalAsyncValue);
+            });
+        });
+
+        return flatAsync;
+    }
+
+
+    function Async() {
+        this.value = null;
+        this.completed = false;
+        this.action = doNothing;
+    }
 
 
     function doNothing(){}
-    //v1 function identity(a){ return a }
 
 }(window.jQuery));
