73cbcfdac470829520cf871c1581c56fc031378b
programaker <programaker@gmail.com>
2016-07-14 23:37:37 -0300

    Fix failure example - jsonp request errors don't trigger error callback, that's why old example didn't work properly

--- fail-page.js
+++ fail-page.js
@@ -1,76 +1,24 @@
 //Page modules can be auto-exec, or else no one will execute them
 (function FailPage($, Async) {
-    var async = Async($);
-
-    //request 4 Youtube channels by id
-    // => Async[List[YoutubeChannel]]
-    var asyncChannels = async.request({
-        url: 'https://www.googleapis.com/youtube/v3/channels',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        data: {
-            key: 'AIzaSyDQOzdypbd04-ExD90xUVPoEG2Hfx7X3X8',
-            part: 'snippet',
-            id: 'UCEWHPFNilsT0IfQfutVzsag,UCsXVk37bltHxD1rDPwtNM8Q,UCvzvWfxeFWZDUH835lMAwEg,UCHCph-_jLba_9atyCZJPLQQ'
+    Async($).request({
+        url: 'http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterTodasPosicoes__ERROR',
+        beforeSend: function logRequestAboutToHappen() {
+            console.log('>>> will make an ajax request...');
         }
-    });
-
-    //choose one of the channels randomly
-    //Async[List[YoutubeChannel]] => Async[YoutubeChannel]
-    var asyncChosenChannel = asyncChannels.map(function chooseRandomChannel(channels) {
-        return channels.items[randomIntBetween(0, 3)];
-    });
-
-    //request the last 10 videos of the chosen channel
-    //Async[YoutubeChannel] => Async[List[YoutubeVideo]]
-    var asyncChannelLastVideos = asyncChosenChannel.flatMap(function requestVideosFromChannel(chosenChannel) {
-        return async.request({
-            url: 'https://www.googleapis.com/youtube/v3/search',
-            dataType: 'jsonp',
-            jsonp: 'callback',
-            data: {
-                key: 'AIzaSyDQOzdypbd04-ExD90xUVPoEG2Hfx7X3X8__ERROR', //<= cause error
-                part: 'snippet',
-                type: 'video',
-                order: 'date',
-                maxResults: 10,
-                channelId: chosenChannel.id
-            }
-        });
-    });
-
-    //get one of the videos randomly
-    //Async[List[YoutubeVideo]] => Async[YoutubeVideo]
-    var asyncChosenVideo = asyncChannelLastVideos.map(function chooseRandomVideo(channelLastVideos) {
-        return channelLastVideos.items[randomIntBetween(0, 9)];
-    });
-
-    //transform the video object in another with only id and high-res thumbnail url
-    //Async[YoutubeVideo] => Async[SimpleVideo]
-    var asyncSimpleVideo = asyncChosenVideo.map(function convertToSimpleVideo(chosenVideo) {
-        return {
-            videoId: chosenVideo.id.videoId,
-            videoThumbnailUrl: chosenVideo.snippet.thumbnails.high.url
-        };
-    });
-
-    //finally, handle the results!
-    asyncSimpleVideo.on({
-        success: function displaySimpleVideo(simpleVideo) {
-            console.log('>>> success => ', simpleVideo);
+    }).on({
+        success: function displaySampleBusPosition(busPositions) {
+            console.log('>>> success => ', busPositions);
+            $('#bus').text(busPositions['COLUMNS'][1] + ' = ' + busPositions['DATA'][0][1]);
             $('#error').text('');
         },
-        error: function logError(e) {
+        error: function displayError(e) {
             console.log('>>> error => ', e);
+            $('#bus').text('');
             $('#error').text(e.textStatus);
         },
         complete: function logCompletion() {
             console.log('>>> done');
         }
     });
-
-    function randomIntBetween(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
 
 }(window.jQuery, window.Async));
